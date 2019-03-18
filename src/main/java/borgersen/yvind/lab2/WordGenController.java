1a318432883a1b61dfbb53a89accd2c2541a2610
Öyvind <oybo84@gmail.com>
2019-03-18 17:31:17 +0100

    Initial commit

---
+++ src/main/java/borgersen/yvind/lab2/WordGenController.java
@@ -0,0 +1,24 @@
+package borgersen.yvind.lab2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WordGenController {
+
+
+    @GetMapping("/words")
+    public String words(Model model){
+        model.addAttribute("words", new WordGenerator());
+        return "words";
+    }
+
+    @PostMapping("/words")
+    public String wordSubmit(@ModelAttribute WordGenerator wordGenerator ){
+        return "result";
+    }
+
+}
---
+++ src/main/java/borgersen/yvind/lab2/WordGenerator.java
@@ -0,0 +1,14 @@
+package borgersen.yvind.lab2;
+
+public class WordGenerator {
+
+    private String words;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+}
